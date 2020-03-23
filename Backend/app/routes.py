49e657e73db967e548b9adba87414335a2b70318
Daniel Kiss <57199540+dislikeNSA@users.noreply.github.com>
2020-03-23 17:36:46 +0100

    flaskenv added

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -3,4 +3,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Hello World!"
+
+@app.route('/potato')
+def potato():
+    return "potato"
