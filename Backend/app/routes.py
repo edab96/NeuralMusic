37002e84eb4a6829544ee2a48306a204c56687f1
Daniel Kiss <57199540+dislikeNSA@users.noreply.github.com>
2020-04-18 09:27:22 +0200

    Flask progress
    added files for flask app

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,10 +1,36 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title = 'Test', user=user, posts = posts)
 
 @app.route('/potato')
 def potato():
     return "potato"
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
